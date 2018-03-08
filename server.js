46f75fc4413cdb474b303f48e3f660d876f798db
Sudhir <sudhirmunna007@gmail.com>
2018-03-07 22:51:53 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+
+let app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+
+// Create a middleware
+app.use((req, res, next) => {
+    let now = new Date().toString();
+    let log = `${now} ${req.method} ${req.path}`;
+    console.log(log);
+    fs.appendFileSync('server.log',log + '\n', (err) => {
+        if(err) {
+            console.log(`Unabe to append to server.log`);
+        }
+    })
+    next();
+});
+
+// app.use((req, res) => {
+//     res.render('maintenance.hbs');
+// });
+// always add static middleware at the end
+app.use(express.static(__dirname + '/public'));
+
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear()
+});
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase()
+});
+
+app.get('/', (req, res) => {
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Whats up dude!!'
+    }); 
+});
+
+app.get('/about', (req,res) => {
+    res.render('about.hbs', {
+        pageTitle: 'About Page'
+    });    
+});
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'not available'
+    })
+});
+
+app.listen(3000);
